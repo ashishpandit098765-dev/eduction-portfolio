c9e1381f5346521357175c071b30e63a3c265e95
ashishpandit098765-dev <ashishpandit098765@gmail.com>
2026-01-12 18:36:49 +0530

    dep1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,5 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import nextConfig from "eslint-config-next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = Array.isArray(nextConfig) ? nextConfig : [nextConfig];
 
 export default eslintConfig;
